076f435c46cb127b82d40aced6e52c7ced513790
Choreas <niklas.kosanke@web.de>
2021-02-10 15:27:15 +0100

    fixtures for questions

--- symfony/src/DataFixtures/CombinedFixtures.php
+++ symfony/src/DataFixtures/CombinedFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Frage;
 use App\Entity\Kurs;
 use App\Entity\Session;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -36,6 +37,29 @@ class CombinedFixtures extends Fixture
         return $session;
     }
 
+    private function createQuestions(Kurs $course): Array {
+        $contents = 
+        [
+            'Ah! Where are you hiding? Happiness is looking for you. Come out in the sunshine. All the darkness will vanish and happiness will surround you.',
+            'But the happiest people are the ones who understand that good things occur when one allows them to.',
+            'Just because you are happy it does not mean that the day is perfect but that you have looked beyond its imperfections',
+            'I do want more. I am not content with being happy. I was not made for it. It is not my destiny. My destiny is the opposite.',
+            'What would it be like to look in the mirror and actually accept what you see? Not loathe the reflection, or despise it, or be resigned to it? But to like it?'
+        ];
+
+        $questions = [];
+
+        foreach ($contents as $content) {
+            $question = new Frage();
+            $question->setKurs($course);
+            $question->setContent($content);
+
+            $questions[] = $question;
+        }
+
+        return $questions;
+    }
+
     public function load(ObjectManager $manager)
     {
         $user = new User();
@@ -46,10 +70,14 @@ class CombinedFixtures extends Fixture
         ));
 
         $newCourse = $this->createCourse($user);
+        $newQuestions = $this->createQuestions($newCourse);
         $newSession = $this->createSession($newCourse);
 
         $manager->persist($user);
         $manager->persist($newCourse);
+        foreach ($newQuestions as $newQuestion) {
+            $manager->persist($newQuestion);
+        }
         $manager->persist($newSession);
         $manager->flush();
     }
