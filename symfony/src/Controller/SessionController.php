c1a228037d8f647dd2f195953e8f40221c74f1bd
Choreas <niklas.kosanke@web.de>
2021-02-14 16:26:00 +0100

    complete basic session workflow

--- symfony/src/Controller/SessionController.php
+++ symfony/src/Controller/SessionController.php
@@ -30,11 +30,57 @@ class SessionController extends AbstractController
     }
 
     /**
-     * @Route("/session/{id}", name="session")
+     * @Route("/session/{code}", name="session")
      */
-    public function goToSession($id, EntityManagerInterface $entityManager): Response
+    public function goToSession($code, EntityManagerInterface $entityManager): Response
     {
-        return $this->render("session/session.html.twig", ['code' => $id]);
+        $session = $entityManager->getRepository(Session::class)->findOneBy(['code' => $code]);
+        $sessionStatus = $session->getStatus();
+        return $this->render("session/session.html.twig", ['code' => $code, 'status' => $sessionStatus]);
+    }
+
+    /**
+     * @Route("/session/{code}/next", name="session_next")
+     */
+    public function nextStep($code, EntityManagerInterface $entityManager): Response
+    {
+        $session = $entityManager->getRepository(Session::class)->findOneBy(['code' => $code]);
+        $sessionStatus = $session->getStatus();
+        $sessionQuestion = $session->getFrage();
+        $course = $session->getKurs();
+        $courseQuestions = $course->getFrages();
+
+        if(!isset($sessionQuestion)) {
+            $sessionQuestion = $courseQuestions[0];
+            $session->setFrage($sessionQuestion);
+            $session->setStatus('QUESTION');
+        } else if ($sessionStatus === 'IDLE') {
+            $nextOrder = $sessionQuestion->getSortorder() + 1;
+            $nextQuestion = null;
+            foreach($courseQuestions as $courseQuestion) {
+                if($courseQuestion->getSortorder() == $nextOrder) {
+                    $nextQuestion = $courseQuestion;
+                    break;
+                }
+            }
+            if(isset($nextQuestion)) {
+                $session->setFrage($nextQuestion);
+                $session->setStatus('QUESTION');
+            } else {
+                $session->setStatus('FINISH');
+                $session->setFrage(null);
+            } 
+        } else if ($sessionStatus === 'QUESTION') {
+            $session->setStatus('RATE');
+        } else if ($sessionStatus === 'RATE') {
+            $session->setStatus('IDLE');
+        }
+
+        $entityManager->persist($session);
+        $entityManager->flush();
+        
+        
+        return $this->redirect("/session/" . $code);
     }
 
     function generateRandomCode($n) {
