9bb409fc66b75b4573a5eadfc31de0ab89359a5a
falk blutau <fbl@upload24.de>
2021-02-15 18:21:38 +0100

    updated ajax calls, class names

--- symfony/public/js/courseselection.js
+++ symfony/public/js/courseselection.js
@@ -1,61 +1,30 @@
 function newCourse() {
-    console.log('newCourse');
-
     $.ajax({
-        url: 'http://localhost/course/add/',
+        url: '/course/add/',
         type: 'POST',
         data: 'New Course',
         success: function(data) {
-            console.log(data)
             if (data != undefined || data != '' || data != []) {
-                console.log('success')
-                window.location.href = `http://localhost/course/edit/${data.id}`;
-            } else {
-                console.log('error');
-                //do stuff
+                window.location.href = '/course/edit/' + data.id;
             }
-        },
-        error: function(request) {
-            console.log(JSON.stringify(request));
         }
     });
 }
 
 function startCourse(id) {
-    console.log('startCourse id: ', id);
-
-    $.ajax({
-        url : 'http://localhost/coursesession',
-        type : 'POST',
-        data : id,
-        success: function(data) {
-            if (data != undefined || data != '' || data != []) {
-                window.location.href = `http://localhost/coursesession/' + ${data.code}`;
-            } else {
-                console.log('error');
-                //do stuff
-            }
-        },
-        error : function(request) {
-            console.log(JSON.stringify(request));
-        }
-    });
-    
+    window.location.href = '/session/start/' + id;
 }
 
 function editCourse(id) {
-    window.location.href = 'http://localhost/course/edit/' + id;
+    window.location.href = '/course/edit/' + id;
 }
 
 function deleteCourse(id) {
-    console.log('deleteCourse id: ', id);
-
     $.ajax({
-        url: `http://localhost/course/delete/${id}`,
+        url: 'http://localhost/course/delete/' + id,
         type: 'GET',
         success: function(data) {
             if (data != undefined || data != '' || data != []) {
-                console.log('deleted successfully');
                 removeCourseElement(id);
             }
         }
